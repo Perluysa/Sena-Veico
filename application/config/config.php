f4d837091fc6eefd62f24c7d55452261d7aab01a
Perluysa <125147367+Perluysa@users.noreply.github.com>
2023-12-02 16:52:37 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -72,7 +72,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] = "http://veicotools.azurewebsites.net" . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])) . '/';
 /*
 |--------------------------------------------------------------------------
